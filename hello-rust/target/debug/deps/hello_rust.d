20d2e1dbf2b2bdd3e59071be2e02e0bb4e0bfb8e
simukarya11 <52550099+simukarya11@users.noreply.github.com>
2020-03-07 06:41:04 -0500

    Add files via upload

---
+++ hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+C:\Users\Simuk\OneDrive\Desktop\Rust\hello-rust\target\debug\deps\hello_rust.exe: src\main.rs
+
+C:\Users\Simuk\OneDrive\Desktop\Rust\hello-rust\target\debug\deps\hello_rust.d: src\main.rs
+
+src\main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+C:\Users\Simuk\OneDrive\Desktop\Rust\hello-rust\target\debug\hello-rust.exe: C:\Users\Simuk\OneDrive\Desktop\Rust\hello-rust\src\main.rs
